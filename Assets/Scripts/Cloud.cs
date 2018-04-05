b9d2585ca8fb76a3e8f65e22750bb89fa9947d0a
asmith <mrpresidentsmith@gmail.com>
2018-04-04 17:03:47 -0700

    Added Tractor.  Changed small cow to Joe.  Started cloud script (not completed).

---
+++ Assets/Scripts/Cloud.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud : MonoBehaviour {
+
+    private float horizontalSpeed;
+
+	// Use this for initialization
+	void Start () {
+        print("lossy scale" + transform.lossyScale);
+        print("local scale" + transform.localScale);
+        ///Determine how to grab the X component of the scale, then make a determination on horizontal speed.    
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Translate position to the right based on speed and time.
+	}
+}
