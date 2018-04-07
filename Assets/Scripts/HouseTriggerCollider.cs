9cc1660b06141fefaf638de6d1cedd3db680efb5
asmith <mrpresidentsmith@gmail.com>
2018-04-07 06:49:39 -0700

    Added transition to interior house scene.

---
+++ Assets/Scripts/HouseTriggerCollider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HouseTriggerCollider : MonoBehaviour {
+
+    private LevelManager levelManager;
+
+    private void Start()
+    {
+        levelManager = FindObjectOfType<LevelManager>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            levelManager.LoadLevel("House");
+        }
+    }
+}
+    
