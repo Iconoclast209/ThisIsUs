bcfe15fb803929e08df01e01386475a0c1f1dfdd
asmith <mrpresidentsmith@gmail.com>
2018-04-07 06:42:36 -0700

    Added cloud movement.

--- Assets/Scripts/Cloud.cs
+++ Assets/Scripts/Cloud.cs
@@ -4,18 +4,32 @@ using UnityEngine;
 
 public class Cloud : MonoBehaviour {
 
+    public float bigCloudSpeed;
+    public float mediumCloudSpeed;
+    public float smallCloudSpeed;
     private float horizontalSpeed;
 
-	// Use this for initialization
-	void Start () {
-        print("lossy scale" + transform.lossyScale);
-        print("local scale" + transform.localScale);
-        ///Determine how to grab the X component of the scale, then make a determination on horizontal speed.    
-        
-	}
+    // Use this for initialization
+    void Start()
+    {
+        if (transform.localScale.x >= 0.9)
+        {
+            horizontalSpeed = bigCloudSpeed;
+        }
+        else if (transform.localScale.x >= 0.5)
+        {
+            horizontalSpeed = mediumCloudSpeed;
+        }
+        else
+        {
+            horizontalSpeed = smallCloudSpeed;
+        }
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		//Translate position to the right based on speed and time.
+	void Update ()
+    {
+        //Translate position to the right based on speed and time.
+        transform.Translate(Vector3.right * horizontalSpeed * Time.deltaTime);
 	}
 }
